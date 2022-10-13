a60e96358b2482ebd25d7a2a6f53d7d22b4a4c58
manhimself26 <rewardmuchech@gmail.com>
2022-10-13 13:49:33 -0700

    e

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+
+
+	write(2, str1, 59);
+
+	return (1);
+
+}	
