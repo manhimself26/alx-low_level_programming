651adc1fc156e9e3761d95339c63e0b12fb36d62
manhimself26 <rewardmuchech@gmail.com>
2022-10-14 15:02:42 -0700

    q

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from zero to nine
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++i)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
+
