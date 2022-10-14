96066c2f8eb74606e8feb2d847d961850d608c2e
manhimself26 <rewardmuchech@gmail.com>
2022-10-14 14:37:03 -0700

    r

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet except e and q.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[24] = "abcdfghijklmnoprstuvwxyz";
+	int i;
+
+	for (i = 0; i < 24; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
+
