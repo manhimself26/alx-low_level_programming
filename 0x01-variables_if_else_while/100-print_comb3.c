fb80f71d922034ac0910668ec88611630dd9f333
manhimself26 <rewardmuchech@gmail.com>
2022-10-17 14:44:37 -0700

    e

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 < 9,num1++)
+	for (num1 = 0; num1 < 9; num1++)
 	{
-		for (num2 = num1 + 1;num2 < 10;num2++)
+		for (num2 = num1 + 1;num2 < 10; num2++)
 		{
 			putchar((num1 % 10) + '0');
 			putchar((num2 % 10) + '0');
