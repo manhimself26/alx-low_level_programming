97dfa4a81ba31dd11510883c429c79e74a9d1335
manhimself26 <rewardmuchech@gmail.com>
2022-10-17 14:39:04 -0700

    e

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two different numbers
+ *
+ * Return: 	Always 0.
+ */
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 < 9,num1++)
+	{
+		for (num2 = num1 + 1;num2 < 10;num2++)
+		{
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 8 && num2 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
