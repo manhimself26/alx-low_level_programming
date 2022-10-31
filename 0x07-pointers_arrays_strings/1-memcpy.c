de304c33baff34c09fc257cdad7f45c0fa12d2f2
manhimself26 <rewardmuchech@gmail.com>
2022-10-31 12:50:47 -0700

    r

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,16 +12,16 @@
  * @src: source buffer to copy characters from.
  * @n: number of bytes to copy from @src.
  *
- * Return: pointer to the destination buffer @dest.
+ * Return: pointer to the destination buffer @dest
  */
-void *_memcpy(void *dest, const void *src, size_t n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *destination = dest;
-	const unsigned char *source = src;
+	unsigned int i;
 
-	for (index = 0; index < n; index++)
-		destination[index] = source[index];
+	for (i = 0; n > 0; i++, n--)
+	{
+		dest[i] = src[i];
+	}
 
 	return (dest);
 }
