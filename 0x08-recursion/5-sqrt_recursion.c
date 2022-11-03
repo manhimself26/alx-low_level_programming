6d7c7eede19b4ea27572722537e8de013e09a14e
manhimself26 <rewardmuchech@gmail.com>
2022-11-03 12:25:09 -0700

    rr

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,10 +9,12 @@ int find_sqrt(int num, int root);
 int _sqrt_recursion(int n);
 
 /**
- * _sqrt_recursion - returns the natural square root of a number 
- * @n: the number to return the square root of.
+ * find_sqrt - finds natural square root of an inputted number.
+ * @num: number to be found square root of.
+ * @root: root to be tested.
  *
- * Return: if n has a natural sqaure root 
+ * Return: if the number has a natural square root -square root
+ * if the number does not have a natural square root - -1.
  */
 int find_sqrt(int num, int root)
 {
@@ -22,14 +24,16 @@ int find_sqrt(int num, int root)
 	if (root == num / 2)
 		return (-1);
 
-	return (find_sqrt(num, root + 1));
+	return (find_sqrt(num, root + 1);
 }
 
+
 /**
- * sqrt_recursion - returns the natural square root of a number
- * @n: the number to return the square root of.
- *
- * Return: if n is natural number
+ * _sqrt_recursion -- returns the natural square root of a number
+ * @n: the number to return square root of
+ * 
+ * Return: if n is a natural square root - the natural square root of n
+ * if n does not have a square root - -1
  */
 int _sqrt_recursion(int n)
 {
@@ -37,9 +41,9 @@ int _sqrt_recursion(int n)
 
 	if (n < 0)
 		return (-1);
-
+	
 	if (n == 1)
-		return (1);
+		return(1);
 
 	return (find_sqrt(n, root);
 }
