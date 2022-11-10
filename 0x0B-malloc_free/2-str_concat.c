9a21775caebbe121d21a6fede05ab90e2ac4a262
manhimself26 <rewardmuchech@gmail.com>
2022-11-10 09:35:17 +0000

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,7 +15,7 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 		s1 = "";
 
-	if (s2 = NULL)
+	if (s2 == NULL)
 		s2 = "";
 
 	for (index = 0; s1[index] || s2[index]; index++)
@@ -27,7 +27,7 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 
 	for (index = 0; s1[index]; index++)
-		concat_str[concat_inex] = s1[index];
+		concat_str[concat_index] = s1[index];
 
 	for (index = 0; s2[index]; index++)
 		concat_str[concat_index] = s2[index];
