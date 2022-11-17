6304de40905b146632bc360f710c87fbbbd5ea25
manhimself26 <rewardmuchech@gmail.com>
2022-11-17 06:20:02 +0000

    function pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,7 +12,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (cmp == NULL || array == NULL || size <= 0)
 		return (-1);
-	for (i = 0; i < size, i++)
+	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
 			return (i);
