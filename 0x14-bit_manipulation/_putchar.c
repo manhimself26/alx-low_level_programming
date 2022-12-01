02be64cb7e6910f4f07547590ec7913601ec224b
manhimself26 <rewardmuchech@gmail.com>
2022-12-01 08:03:14 -0800

    bit manipulation

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ *
+ * Return: on success 1
+ * o error, -1 is returned, and errno is set approximately
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
